a1a805d6343cec8837670a57079c3ce435f18225
Yazan Stash <yazan.stash@mawdoo3.com>
2019-10-10 10:05:29 +0300

    Initital Commit

---
+++ src/Csv.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Stash\Csv;
+
+class Csv
+{
+    protected $file;
+    
+    protected $filePath;
+
+    protected $delimiter = ",";
+
+    protected $enclosure = '"';
+
+    protected $escapeCharacter = "\\";
+
+    public static function create(string $filePath = null)
+    {
+        $filePath = $filePath ?? sprintf('/tmp/%s.csv', static::randomName());
+
+        return new static($filePath);
+    }
+
+    protected function __construct(string $filePath)
+    {
+        $this->filePath = $filePath;
+
+        $this->file = fopen($filePath, 'w');
+
+        if ($this->file === false) {
+            throw new \Exception("Could not open file: $filePath for writing.", 1);
+        }
+    }
+
+    public function write(array $fields)
+    {
+        $result = fputcsv($this->file, $fields, $this->delimiter, $this->enclosure, $this->escapeCharacter);
+
+        if (false === $result) {
+            throw new \Exception("Could not write to file: $this->filePath.", 1);
+        }
+    }
+
+    public function filePath()
+    {
+        return $this->filePath;
+    }
+
+    public function delimiter($delimiter = null)
+    {
+        if (is_null($delimiter)) {
+            return $this->delimiter;
+        }
+
+        $this->delimiter = $delimiter;
+
+        return $this;
+    }
+
+    public function enclosure($enclosure = null)
+    {
+        if (is_null($enclosure)) {
+            return $this->enclosure;
+        }
+
+        $this->enclosure = $enclosure;
+
+        return $this;
+    }
+
+    public function escapeCharacter($escapeCharacter = null)
+    {
+        if (is_null($escapeCharacter)) {
+            return $this->escapeCharacter;
+        }
+
+        $this->escapeCharacter = $escapeCharacter;
+
+        return $this;
+    }
+
+    public static function randomName($length = 40)
+    {
+        $string = '';
+
+        while (($len = strlen($string)) < $length) {
+            $size = $length - $len;
+
+            $bytes = random_bytes($size);
+
+            $string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
+        }
+
+        return $string;
+    }
+    
+    public function closeFile()
+    {
+        return fclose($this->file);
+    }
+
+    public function __destruct()
+    {
+        $this->closeFile();
+    }
+}
---
+++ tests/CsvTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Stash\Csv\Tests;
+
+use Stash\Csv\Csv;
+use PHPUnit\Framework\TestCase;
+
+class CsvTest extends TestCase
+{
+    /** @test */
+    public function the_random_file_name_is_40_character_long()
+    {
+        $this->assertEquals(40, strlen(str_replace('.csv', '', Csv::randomName())));
+    }
+
+    /** @test */
+    public function not_passing_a_file_name_will_generate_a_random_one()
+    {
+        $this->assertStringStartsWith('/tmp/', Csv::create()->filePath());
+        $this->assertStringEndsWith('.csv', Csv::create()->filePath());
+    }
+
+    /** @test */
+    public function passing_a_file_name_will_use_it()
+    {
+        $this->assertEquals('/tmp/filename.csv', Csv::create('/tmp/filename.csv')->filePath());
+    }
+
+    /** @test */
+    public function passing_an_invalid_path_will_throw_an_exception()
+    {
+        try {
+            Csv::create('/root/tmp.csv');
+        } catch (\Exception $e) {
+            $this->assertStringContainsString('failed to open stream: Permission denied', $e->getMessage());
+            return;
+        }
+
+        $this->fail('Expected an exception to be thrown, but did not.');
+    }
+
+    /** @test */
+    public function can_write_array_to_file()
+    {
+        $file = Csv::create('/tmp/test-file.csv');
+
+        $file->write(['Hello', 'Csv']);
+
+        $this->assertEquals("Hello,Csv\n", file_get_contents('/tmp/test-file.csv'));
+    }
+
+    /** @test */
+    public function filePath_returns_the_currently_opened_file_path()
+    {
+        $this->assertEquals('/tmp/random-file.csv', Csv::create('/tmp/random-file.csv')->filePath());
+    }
+
+    /** @test */
+    public function calling_delimiter_without_aguments_returns_the_current_one()
+    {
+        $this->assertEquals(',', Csv::create()->delimiter());
+    }
+
+    /** @test */
+    public function calling_delimiter_with_aguments_changes_it()
+    {
+        $this->assertEquals('|', Csv::create()->delimiter('|')->delimiter());
+    }
+
+    /** @test */
+    public function calling_enclosure_without_aguments_returns_the_current_one()
+    {
+        $this->assertEquals('"', Csv::create()->enclosure());
+    }
+
+    /** @test */
+    public function calling_enclosure_with_aguments_changes_it()
+    {
+        $this->assertEquals("'", Csv::create()->enclosure("'")->enclosure());
+    }
+
+    /** @test */
+    public function calling_escapeCharacter_without_aguments_returns_the_current_one()
+    {
+        $this->assertEquals('\\', Csv::create()->escapeCharacter());
+    }
+
+    /** @test */
+    public function calling_escapeCharacter_with_aguments_changes_it()
+    {
+        $this->assertEquals("|", Csv::create()->escapeCharacter("|")->escapeCharacter());
+    }
+}
